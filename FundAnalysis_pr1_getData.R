ef7c7f0b15b9a4267f005189eac1c88bd9b7d0fd
Chechuco <josechu.carballedo@gmail.com>
2016-02-12 17:21:31 +0100

    Cambios menores

--- FundAnalysis_pr1_getData.R
+++ FundAnalysis_pr1_getData.R
@@ -1,14 +1,16 @@
 ## Open libraries
 library(googleVis)
+library(scales)
 
 # Set the root path.
 #path <- "./FundAnalysis_pr1/"
 
-# Get tha data
+# Get and clean data
 #funds <- read.csv(paste0(path, "FundAnalysis_pr1.csv"), header=TRUE)
 Funds <- read.csv("FundAnalysis_pr1.csv", header=FALSE)
 colnames(Funds) <- c("Fecha", "Date", "ISIN", "Fund", "AssetClass_2", "Currency", "Price", "AuM", "SPX", "US10Y")
-Funds$Fecha <- as.Date(Funds$Fecha)
+Funds$Fecha <- as.Date(Funds$Fecha,format='%m/%d/%Y')
 
 #Plot the chart
 Motion=gvisMotionChart(Funds, idvar = "ISIN", timevar = "Fecha", xvar = "US10Y", yvar = "Price", colorvar = "AssetClass_2", date.format = "%Y%m%d")
+plot(Motion)
