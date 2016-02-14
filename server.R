78753ddafa42b0547e8409360eb0670227c8adf9
chechuco <josechu.carballedo@gmail.com>
2016-02-15 00:15:36 +0100

    Grafico definitivo

--- server.R
+++ server.R
@@ -1,15 +1,23 @@
 library(googleVis)
 
 shinyServer(function(input, output) {
-   datasetInput <- reactive({
-     switch(input$dataset,
-        "Products" = Products,
-        "pressure" = pressure,
-        "cars" = cars)
+   assetClass_2Input <- reactive({
+     switch(input$assetClass_2,
+        "All" = Products,
+        "Short term" = Products[Products$AssetClass_2 == "RF Corto plazo",],
+        "Corporate" = Products[Products$AssetClass_2 == "RF Corporativa",],
+        "Flexible EUR" = Products[Products$AssetClass_2 == "RF Flexible EUR",],
+        "Flexible others" = Products[Products$AssetClass_2 == "RF Flexible Otros",],
+        "High Yield" = Products[Products$AssetClass_2 == "RF High Yield",],
+        "Emerging" = Products[Products$AssetClass_2 == "RF Emergente",],
+        "Others" = Products[Products$AssetClass_2 == "RF Otros",])
    })
 
   output$view <- renderGvis({
      #gvisScatterChart(datasetInput())
-    gvisMotionChart(datasetInput(), idvar = "ID", timevar = "Fecha", xvar = "US10Y", yvar = "Price", sizevar = "AuM", colorvar = "AssetClass_1", date.format = "%Y%m%d")
+    gvisMotionChart(assetClass_2Input(), idvar = "ISIN", timevar = "Fecha", xvar = "US10Y", yvar = "Price", sizevar = "AuM", colorvar = "AssetClass_2", date.format = "%Y%m%d")
   })
 })
+
+
+
--- ui.R
+++ ui.R
@@ -1,17 +1,16 @@
 library(shiny)
-library(ggplot2)
 library(googleVis)
 
 # Get data (run in local computer)
-Products <- read.csv("AllProducts12M_pr1.csv", header=FALSE, na.strings = c("N/A", "\\N"))
-colnames(Products) <- c("Fecha", "ID", "ISIN", "Product", "AssetClass_1", "AssetClass_2", "Type", "Currency", "Price", "AuM", "SPX", "US10Y")
+Products <- read.csv("FundAnalysis_pr1.csv", header=FALSE, na.strings = c("N/A", "\\N"))
+colnames(Products) <- c("Fecha", "ISIN", "Product", "AssetClass_2", "Currency", "Price", "AuM", "SPX", "US10Y")
 Products$Fecha <- as.Date(Products$Fecha)
 
 shinyUI(pageWithSidebar(
-  headerPanel("Financial products explorer"),
+  headerPanel("PIMCO Fixed Income Funds Explorer"),
   sidebarPanel(
-    selectInput("dataset", "Choose a dataset:",
-        choices = c("Products", "pressure", "cars"))
+    selectInput("assetClass_2", "Choose an asset class:",
+        choices = c("All", "Short term", "Corporate", "Flexible EUR", "Flexible others", "High Yield", "Emerging", "Others"))
     ),
     mainPanel(
       htmlOutput("view")
