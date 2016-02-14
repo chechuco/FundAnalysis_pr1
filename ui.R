ebf953338798da44b2c8b31bb8604dda9ce93f33
chechuco <josechu.carballedo@gmail.com>
2016-02-14 14:14:37 +0100

    Tres datasets

--- ui.R
+++ ui.R
@@ -2,33 +2,18 @@ library(shiny)
 library(ggplot2)
 library(googleVis)
 
-#Get data
-Products <- read.csv("ProductFeatures_pr1.csv", header=FALSE)
-colnames(Products) <- c("Product", "AssetClass_1", "AssetClass_2", "Type", "Currency", "IsComplex", "TER")
+# Get data (run in local computer)
+Products <- read.csv("AllProducts12M_pr1.csv", header=FALSE, na.strings = c("N/A", "\\N"))
+colnames(Products) <- c("Fecha", "ID", "ISIN", "Product", "AssetClass_1", "AssetClass_2", "Type", "Currency", "Price", "AuM", "SPX", "US10Y")
+Products$Fecha <- as.Date(Products$Fecha)
 
-dataset <- Products
-
-fluidPage(
-  
-  titlePanel("Funds Analysis"),
-  
+shinyUI(pageWithSidebar(
+  headerPanel("Financial products explorer"),
   sidebarPanel(
-    
-    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
-                value=min(19, nrow(dataset)), step=5, round=0),
-    
-    selectInput('x', 'X', names(dataset)),
-    selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
-    selectInput('color', 'Color', c('None', names(dataset))),
-    
-    checkboxInput('jitter', 'Jitter'),
-    checkboxInput('smooth', 'Smooth'),
-    
-    selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
-    selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
-  ),
-  
-  mainPanel(
-    plotOutput('plot')
-  )
-)
+    selectInput("dataset", "Choose a dataset:",
+        choices = c("Products", "pressure", "cars"))
+    ),
+    mainPanel(
+      htmlOutput("view")
+    )
+))
